34dbb5fadddf5d46cf8042f3b1e5fa5d1ef627ff
A-nupha supreyaporn <a-nupha.sup.it58@cpc.ac.th>
2018-11-19 13:22:04 +0700

    add project

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app" dark>
+
+      <v-layout ma-1 md12>
+        <v-flex justify-center>
+      <v-toolbar
+        color="blue darken-1"
+        dark
+        tabs
+      >
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+
+        <v-toolbar-title>NAV LINK</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <!-- <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn> -->
+
+        <!-- <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn> -->
+
+        <v-tabs
+          slot="extension"
+          v-model="model"
+          centered
+          color="blue darken-2"
+          slider-color="yellow"
+        >
+          <v-tab
+            v-for="i in 3"
+            :key="i"
+            :href="`#tab-${i}`"
+          >
+            Item {{ i }}
+          </v-tab>
+        </v-tabs>
+      </v-toolbar>
+
+      <v-tabs-items v-model="model">
+        <v-tab-item
+          v-for="i in 3"
+          :id="`tab-${i}`"
+          :key="i"
+        >
+          <v-card flat>
+            <v-card-text v-text="text"></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      </v-flex>
+      </v-layout>
+    <router-view/>
+  </div>
+</template>
+<script>
+import HOME from './views/Home.vue'
+
+export default {
+  data() {
+    return {
+      model: 'tab-1',
+      componen: HOME,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+
+  <div>
+    datatestbarcodeScenner:{{databarcode}}
+    <!-- <v-text-field v-model="databarcode"></v-text-field> -->
+    <!-- <quagga-scanner v-bind=" readerSize={width:640,height:480}" :onDetected="logIt"></quagga-scanner> -->
+    <quagga-scanner v-if="databarcode != null"    :onDetected="logIt" :readerSize="readerSize" :readerType="'ean_reader'"></quagga-scanner>
+    <!-- <v-flex v-else>
+      <v-text-field v-model="databarcode"></v-text-field>
+    </v-flex> -->
+    <!-- <quagga-scanner v-else="databarcode != null"    :onDetected="logIt" :readerSize="readerSize" :readerType="'ean_reader'"></quagga-scanner> -->
+    <!-- <quagga-scanner :onDetected="logIt" :readerSize="readerSize" :readerType="'ean_reader'"></quagga-scanner> -->
+  </div>
+</template>
+
+<script>
+import { QuaggaScanner } from 'vue-quaggajs';
+// import js from './js/index.js';
+// import Quagga from 'quagga';
+
+export default {
+  name: 'VueBarcodeTest',
+  data() {
+    return {
+      model: 'tab-2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      decoder: {
+        readers: ['ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader', '2of5_reader', 'code_93_reader'],
+      },
+      databarcode: [],
+      readerSize: {
+        width: 640,
+        height: 480,
+      },
+      detecteds: [],
+    };
+  },
+  components: {
+    QuaggaScanner,
+    // eslint-disable-next-line vue/no-unused-components
+    // Quagga=\63\\,
+  },
+  methods: {
+    // Quagga.init({
+    // inputStream : {
+    //   name : "Live",
+    //   type : "LiveStream",
+    //   target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+    // },
+    // decoder : {
+    //   readers : ["code_128_reader"]
+    // }
+  // },
+    // function(err) {
+    //   if (err) {
+    //     console.log(err);
+    //     return
+    //   }
+    //   console.log('Initialization finished. Ready to start');
+    //   Quagga.start();
+    // },
+    logIt(data) {
+      // console.log('detected', data)
+      // this.databarcode = this.data
+
+      console.log('detectedbarcode', data.codeResult.code)
+      // alert(data.codeResult.code)
+      this.databarcode = data.codeResult.code
+      console.log(data)
+
+      // this.databarcode = this.data.codeResult.code
+      // console.log(this.databarcode)
+    },
+
+
+  },
+};
+</script>
